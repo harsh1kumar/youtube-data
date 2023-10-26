a5545bad8dfad60d9e88c5d22c5d487c2096c1ba
Harsh Kumar <harsh1kumar@users.noreply.github.com>
2023-10-26 10:39:59 +0530

    Add function descriptions in utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,9 @@ from google.oauth2 import service_account
 
 
 def get_service_account_cred():
-
+    """
+    Returns GCP credentials for the service account
+    """
     service_account_json = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
     credentials = service_account.Credentials.from_service_account_file(
         service_account_json,
